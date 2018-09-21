b957cf025abde5d48e3878d1983d892389360841
Mona Lisa <amarte3293@yahoo.com>
2018-09-21 13:32:47 -0400

    initial push

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Deadpool',
+		photo: 'https://i.kinja-img.com/gawker-media/image/upload/s--HApVmIBh--/c_scale,f_auto,fl_progressive,q_80,w_800/zigjxvw4cg3xbabifaet.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Light Yagami',
+		photo: 'http://static.comicvine.com/uploads/original/11111/111111327/3278748-light-yagami-wallpaper_145917-1024x768.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Goku',
+		photo: 'https://www.geek.com/wp-content/uploads/2017/08/Dragon-Ball-Super-Goku-625x352.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Kirito',
+		photo: 'https://cdn1-33b9.kxcdn.com/wp-content/uploads/2015/06/Kirito1.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Meliodas',
+		photo: 'https://vignette.wikia.nocookie.net/nanatsu-no-taizai/images/0/02/Meliodas_announcing_they_are_the_Seven_Deadly_Sins.png/revision/latest?cb=20141221142135',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Shoyo Hinata',
+		photo: 'https://vignette.wikia.nocookie.net/haikyuu/images/b/b3/Hinata%27s_joy_being_in_a_clubroom.PNG/revision/latest?cb=20140831230446',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Naruto Uzamaki',
+		photo: 'https://vignette.wikia.nocookie.net/naruto/images/4/42/Naruto_Part_III.png/revision/latest/scale-to-width-down/300?cb=20180117103539',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Ichigo Kurosaki',
+		photo: 'https://static.comicvine.com/uploads/original/14/148711/3590808-6705700691-Ichig.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Edward Elric',
+		photo: 'https://vignette.wikia.nocookie.net/fma/images/b/bc/Edward_Elric.jpg/revision/latest?cb=20110526194029',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Yugi Muto',
+		photo: 'https://vignette.wikia.nocookie.net/playstationallstarsbattleroyale/images/4/47/Yami_yugi_colored_by_pinkycute03-d5nkmc9.png/revision/latest?cb=20130118065117',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
